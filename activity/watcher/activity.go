d404fdcb524cdafd52c00b3c33db2d9b55ebcb88
malwxd <31409192+malwxd@users.noreply.github.com>
2017-12-06 17:54:22 +0100

    Update activity.go

--- activity/watcher/activity.go
+++ activity/watcher/activity.go
@@ -26,11 +26,11 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
 
 	// do eval
         name := context.GetInput("name").(string)
-        logger.Debugf("Hi [%s]", name)
+        //logger.Debugf("Hi [%s]", name)
         cTime := time.Now()  
-        logger.Debugf("Now is: [%s]", cTime)
+       // logger.Debugf("Now is: [%s]", cTime)
         onTime := time.Date(cTime.Year(), cTime.Month(), cTime.Day(), 10, 30, 00, 000000000, time.UTC)
-        logger.Debugf("Now is: [%s]", onTime) 	
+       // logger.Debugf("Now is: [%s]", onTime) 	
         late := cTime.After(onTime)
         context.SetOutput("result", late)
 	return true, nil
